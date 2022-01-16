6d83647010052111a926fc7ba883246dc24119f6
kopalgarg <kopalgarg24@gmail.com>
2022-01-15 23:30:35 -0700

    Update a1_preproc.py

--- assignment_1/provided_files/code/a1_preproc.py
+++ assignment_1/provided_files/code/a1_preproc.py
@@ -8,6 +8,7 @@
 #  * All of the files in this directory and all subdirectories are:
 #  * Copyright (c) 2020 Frank Rudzicz
  
+## Kopal Garg, 1003063221
 
 import sys
 import argparse
@@ -16,7 +17,7 @@ import json
 import re
 import spacy
 
-
+# python3 -m spacy download en_core_web_sm
 nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
 nlp.add_pipe('sentencizer')
 
@@ -32,30 +33,48 @@ def preproc1(comment , steps=range(1, 6)):
         modComm : string, the modified comment 
     '''
     modComm = comment
+
     if 1 in steps:  
         #modify this to handle other whitespace chars.
-        #replace newlines with spaces
+        # 1.A replace newlines with spaces  
         modComm = re.sub(r"\n{1,}", " ", modComm)
+        # 1.B remove trailing and leading spaces
+        modComm = re.sub(r"^\s+|\s+$", "", modComm)
+        # 1.C remove tabs, new lines, carriage returns
+        modComm = re.sub(r"\n+|\t+|\r+", " ", modComm)
 
     if 2 in steps:  # unescape html
-        print("TODO")
+        modComm = htmp.unescape(modComm)
 
     if 3 in steps:  # remove URLs
         modComm = re.sub(r"(http|www)\S+", "", modComm)
         
     if 4 in steps: #remove duplicate spaces.
-        print("TODO") 
+        modComm = re.sub(r"\s+", " ", modComm)
 
     if 5 in steps:
         print("TODO")
+
         # TODO: get Spacy document for modComm
-        
+        utt = nlp(modComm)
+
         # TODO: use Spacy document for modComm to create a string.
         # Make sure to:
-        #    * Insert "\n" between sentences.
         #    * Split tokens with spaces.
+
+        for sentence in utt.sents:
+            tokens = []
+            for token in sentence:
+                text = token.text
+                
         #    * Write "/POS" after each token.
-            
+                posTagged = text + '/' + token.tag_
+                tokens.append(posTagged)
+
+        #    * Insert "\n" between sentences.
+            processedLine += " ".join(tokens) + '\n'
+
+
     
     return modComm
 
@@ -70,12 +89,24 @@ def main(args):
             data = json.load(open(fullFile))
 
             # TODO: select appropriate args.max lines
-            # TODO: read those lines with something like `j = json.loads(line)`
-            # TODO: choose to retain fields from those lines that are relevant to you
-            # TODO: add a field to each selected line called 'cat' with the value of 'file' (e.g., 'Alt', 'Right', ...) 
-            # TODO: process the body field (j['body']) with preproc1(...) using default for `steps` argument
-            # TODO: replace the 'body' field with the processed text
-            # TODO: append the result to 'allOutput'
+            start_index = int(args.ID[0] % len(data))
+            end_index =  int(start_index + args.max)
+            for i in range(start_index, end_index, 1):
+                # TODO: read those lines with something like `j = json.loads(line)`
+                comment = json.loads(data[i])
+                # TODO: choose to retain fields from those lines that are relevant to you
+                comment_fields = {}
+                comment_fields['id'] = comment['id']
+                comment_fields['subreddit'] = comment['subreddit']
+                comment_fields['body'] = comment['body']
+                # TODO: add a field to each selected line called 'cat' with the value of 'file' (e.g., 'Alt', 'Right', ...) 
+                comment_fields['cat'] = file
+                # TODO: process the body field (j['body']) with preproc1(...) using default for `steps` argument
+                processed_body = preproc1(comment_fields['body'], steps = range(0,1))
+                # TODO: replace the 'body' field with the processed text
+                comment_fields['body'] = processed_body
+                # TODO: append the result to 'allOutput'
+                allOutput.append(comment_fields)
             
     fout = open(args.output, 'w')
     fout.write(json.dumps(allOutput))
