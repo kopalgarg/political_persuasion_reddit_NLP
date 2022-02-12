904b51ca27bec0585adbe5d84e1e3e6a17fb85f2
Kopal Garg <kopalgarg24@gmail.com>
2022-02-12 13:19:47 -0700

    Update a1_classify.py

--- assignment_1/provided_files/code/a1_classify.py
+++ assignment_1/provided_files/code/a1_classify.py
@@ -8,6 +8,9 @@
 #  * All of the files in this directory and all subdirectories are:
 #  * Copyright (c) 2020 Frank Rudzicz
 
+## Kopal Garg, 1003063221
+#!/usr/bin/env python
+
 import argparse
 import os
 from scipy.stats import ttest_rel
@@ -16,10 +19,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import f_classif
 from sklearn.feature_selection import SelectKBest
 from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.neural_network import MLPClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import SGDClassifier  
+from scipy import stats
 
 # set the random state for reproducibility 
 import numpy as np
@@ -28,7 +32,7 @@ np.random.seed(401)
 def accuracy(C):
     ''' Compute accuracy given Numpy array confusion matrix C. Returns a floating point value '''
     # deal with cases where the denominator could be 0
-    if np.sum(C) = 0:
+    if np.sum(C) == 0:
         return 0
     else:
         return np.trace(C)/np.sum(C)
@@ -70,22 +74,23 @@ def class31(output_dir, X_train, X_test, y_train, y_test):
        i: int, the index of the supposed best classifier
     '''
     print('TODO Section 3.1')
-
+    # keep track of best model index
     iBest = None
+    # keep track of best model acc
     best_acc = 0
     
     with open(f"{output_dir}/a1_3.1.txt", "w") as outf:
-        # For each classifier, compute results and write the following output:
-        #     outf.write(f'\tAccuracy: {acc:.4f}\n')
-        #     outf.write(f'\tRecall: {[round(item, 4) for item in recall]}\n')
-        #     outf.write(f'\tPrecision: {[round(item, 4) for item in precision]}\n')
-        #     outf.write(f'\tConfusion Matrix: \n{conf_matrix}\n\n')
 
         for i, model in enumerate(listModels):
+            # get classifier name
             classifier_name = model.__class__.__name__
+            # fit on full training set
             model.fit(X_train, y_train)
+            # predictions on full test set
             y_test_pred = model.predict(X_test)
+            # confusion matrix using test pred  y and test y
             conf_matrix = confusion_matrix(y_test, y_test_pred)
+            # accuracy, precision and recall from conf matrix
             accuracies = accuracy(conf_matrix)
             precisions = precision(conf_matrix)
             recalls = recall(conf_matrix)
@@ -95,10 +100,14 @@ def class31(output_dir, X_train, X_test, y_train, y_test):
             outf.write(f'\tRecall: {[round(item, 4) for item in recalls]}\n')
             outf.write(f'\tPrecision: {[round(item, 4) for item in precisions]}\n')
             outf.write(f'\tConfusion Matrix: \n{conf_matrix}\n\n')
-
+            # if curr acc > best acc, replace best acc with curr acc
             if accuracies > best_acc:
                 iBest = i 
                 best_acc = accuracies
+            # to keep track of training progress
+            print("Trained", classifier_name)
+            print(i, accuracies)
+
     return iBest +1
         
 def class32(output_dir, X_train, X_test, y_train, y_test, iBest):
@@ -117,30 +126,39 @@ def class32(output_dir, X_train, X_test, y_train, y_test, iBest):
        y_1k: numPy array, just 1K rows of y_train
    '''
     print('TODO Section 3.2')
-    iBest = iBest-1 # indexing
-    best_model = models[iBest]
+    # get the best index from 3.1. 
+    iBest = iBest-1 # indexing differently so subtract 1
+    # get the best model using the index
+    best_model = listModels[iBest]
+    # different training set sizes to try
     dataset_sizes = [1000, 5000, 10000, 20000]
     accuracies = []
-
+    # loop over all different training sizes 
     for size in dataset_sizes:
+        # index random ints of size size
         index_ = np.random.choice(a  = X_train.shape[0], size = size)
+        # use these indices to subset the training set
         X_sub = X_train[index_]
         y_sub = y_train[index_]
-
-        best.fit(X_sub, y_sub)
-        y_pred_test = best.predict(X_test)
+        # fit the best model on subset of the training set
+        best_model.fit(X_sub, y_sub)
+        # make predictions on full test set
+        y_pred_test = best_model.predict(X_test)
+        # get conf matrix of y test pred and y test
         C = confusion_matrix(y_test, y_pred_test)
+        # compute accuracy
         accuracies.append(accuracy(C))
-        if s==1000:
+        # if size of the set if 1000, set the sets we're going to return to the current training subset
+        if size==1000:
             X_1k = X_sub
             y_1k = y_sub
-
+        
     with open(f"{output_dir}/a1_3.2.txt", "w") as outf:
         # For each number of training examples, compute results and write
         # the following output:
         #     outf.write(f'{num_train}: {acc:.4f}\n'))
         for i, size in enumerate(dataset_sizes):
-            outf.write(f'{size}: {accuracies:.4f}\n'))
+            outf.write(f'{size}: {accuracies[i]:.4f}\n')
 
     return (X_1k, y_1k)
 
@@ -159,52 +177,57 @@ def class33(output_dir, X_train, X_test, y_train, y_test, i, X_1k, y_1k):
        y_1k: numPy array, just 1K rows of y_train (from task 3.2)
     '''
     print('TODO Section 3.3')
-    best_model = models[i-1]
-    p_value = []
+    best_model = listModels[i-1]
     with open(f"{output_dir}/a1_3.3.txt", "w") as outf:
-        # Prepare the variables with corresponding names, then uncomment
-        # this, so it writes them to outf.
-        for k in [5,50]:
-            sel = SelectKBest(f_classif, k=k)
-            X_new = sel.fit_transform(X_train, y_train) # 1k rows from 3.2
-            index_ = sel.p_values_
-            index_ = index_.argsort()
-            p_values = sel.pvalues_[index_]
-
-            # for each number of features k_feat, write the p-values for
-            # that number of features:
+        # Prepare the variables with corresponding names, then uncomment this, so it writes them to outf.
+        # for each number of features k_feat, write the p-values for that number of features:
+        for k_feat in [5,50]:
+            # best k features
+            sel = SelectKBest(score_func=f_classif, k = k_feat)
+            # fit and transform the full train 32k set
+            X_new  = sel.fit_transform(X_train, y_train)
+            # pvalue of the features 
+            p_values = sel.pvalues_
+            # arg sort the p values and using indices get the pvalues of top k features 
+            p_values = p_values[p_values.argsort()] # top k p-values
+            # output to the main file
             outf.write(f'{k_feat} p-values: {[format(pval) for pval in p_values]}\n')
-            ix_32k = sel.pvalues_.argsort()[:k]
-            outf.write(f'{k} best features: {ix_32k}\n')
 
-            if k==5:
-                # 1000
-                X_new5 = sel.fit_transform(X_1k, y_1k)
-                best_model.fit(X_new5, y_1k)
-                y_predict5 = best_model.predict(sel.transform(X_test))
-                C = confusion_matrix(y_test, y_predict5)
-                accuracy_1k = accuracy(C)
-                outf.write(f'Accuracy for 1k: {accuracy_1k:.4f}\n')
+        accuracies = [] # [0] would be 1k and [1] would be for 32k
+        
+        for i in ['1k','full']:
+            if i == '1k': X = X_1k; y = y_1k
+            else: X = X_train; y = y_train
+            # best 5 features
+            sel = SelectKBest(score_func=f_classif, k = 5)
+            # fit and transform the 1k and 32k sets
+            X_new = sel.fit_transform(X, y)
+            # transform the full test set
+            X_test_new = sel.transform(X_test)
+            # fit the best model from part 1
+            best_model.fit(X_new, y)
+            # predict the y value using the transformed test set
+            y_test_pred = best_model.predict(X_test_new)
+            # build confusion matrix using test pred y and test y values
+            conf_matrix = confusion_matrix(y_test, y_test_pred)
+            # get the acc from the conf matrix
+            accuracies.append(accuracy(conf_matrix))
+        accuracy_1k =accuracies[0]; accuracy_full = accuracies[1]
+        outf.write(f'Accuracy for 1k: {accuracy_1k:.4f}\n')
 
-                ix_1k = sel.pvalues_.argsort()[:k]
-                print('ix_1k', ix_1k)
-
-                # 32,000
-                X_new32 = sel.fit_transform(X_train, y_train)
-                best_model.fit(X_new32, y_train)
-                y_predict32 =best.model(sel.transform(X_test))
-                C = confusion_matrix(y_test, y_predict32)
-                accuracy_32k = accuracy(C)
-                outf.write(f'Accuracy for full dataset: {accuracy_32k:.4f}\n')
-
-                ix_32k = sel.pvalues_.argsort()[:k]
-                print('ix_32k', ix_32k)
-
-                feature_intersection = set(ix_1k) & set(ix_32k)
-                outf.write(f'Chosen feature intersection: {feature_intersection}\n')
-                outf.write(f'Top-5 at higher: {ix_32k}\n')
-        pass
-
+        outf.write(f'Accuracy for full dataset: {accuracy_full:.4f}\n')
+        # feature intersection
+        k_feat = 5
+        ix_1k = SelectKBest(score_func = f_classif,
+                            k = k_feat).fit(X_1k, y_1k).get_support(indices= True)
+        ix_full = SelectKBest(score_func = f_classif,
+                            k = k_feat).fit(X_train, y_train).get_support(indices= True)
+        feature_intersection = set(ix_1k) & set(ix_full)
+        outf.write(f'Chosen feature intersection: {feature_intersection}\n')
+        # top k = 5 feature indices extracted for full dataset
+        top_5 = set(ix_full) # write as a set 
+        outf.write(f'Top-5 at higher: {top_5}\n')
+    pass
 
 def class34(output_dir, X_train, X_test, y_train, y_test, i):
     ''' This function performs experiment 3.4
@@ -218,36 +241,56 @@ def class34(output_dir, X_train, X_test, y_train, y_test, i):
        i: int, the index of the supposed best classifier (from task 3.1)  
         '''
     print('TODO Section 3.4')
+    
+    # since using the full dataset, vertically stack the train and test sets that we initially did a split on
     X = np.vstack((X_train, X_test))
-    y = np.concatenate((y_train, x_train), axis= 0)
-    k =5 
-    kfold = Kfold(k, shuffle = True, random_state=401)
-    accuracy_models = []
+    # do the same for the lables (making sure they're stacked in the same order)
+    y = np.concatenate((y_train, y_test), axis= 0)
+    # k
+    k=6
+    # do a random split but set the random state to 401 for reproducibility
+    kfold = KFold(k, shuffle = True, random_state=401)
+    # matrix of acuracies  (col=classifier, row=fold)
+    accuracy_models = np.zeros((k, len(listModels)))
+    
     with open(f"{output_dir}/a1_3.4.txt", "w") as outf:
         # Prepare kfold_accuracies, then uncomment this, so it writes them to outf.
         # for each fold:
         #     outf.write(f'Kfold Accuracies: {[round(acc, 4) for acc in kfold_accuracies]}\n')
         # outf.write(f'p-values: {[format(pval) for pval in p_values]}\n')
-        for i, model in enumerate(models):
-            accuracy_i = []
-            for test, train in kf.split(X):
-                X_train = X[train]
-                X_test = X[test]
-                y_train = y[train]
-                y_test = y[test]
+        for i, model in enumerate(listModels):
+            # taking random kfold splits 
+            j=0 # j is fold, i is model
+            for test, train in kfold.split(X):
+                X_train = X[train];X_test = X[test]
+                y_train = y[train];y_test = y[test]
+                # fit model on the training split
                 model.fit(X_train, y_train)
-                y_test_pred = mode.predict(X_test)
+                # test predictions on the testing split
+                y_test_pred = model.predict(X_test)
+                # create a conf matrix, and get the accuracy
                 C = confusion_matrix(y_test, y_test_pred)
-                accuracy_i.append(accuracy(C))
-            accuracy_models.append(accuracy_i)
-        mean_acc = []
-        for acc in accuracy_models:
-            mean_acc.append(np.mean(acc))
-
+                acc = accuracy(C)
+                accuracy_models[j,i] = acc
+                j+=1 # increment for split
+            # compute the mean acc using the top list we appened all acc to
+        
+        for j in accuracy_models:
+            outf.write(f'Kfold Accuracies: {[round(acc, 4) for acc in j]}\n')
+        import pdb; pdb.set_trace()
+        
+        # p-value calculations
+        # S = stats.ttest_rel(a, b)
+        p_values = []
+        iBest =i-1
+        for ind, model in enumerate(listModels):
+            if not listModels[iBest]==model:
+                S = stats.ttest_rel(accuracy_models[:, ind], accuracy_models[:, iBest])
+                p_values.append(S.pvalue)
+        outf.write(f'p-values: {[format(pval) for pval in p_values]}\n')
+        pass
         
 
-
-    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help="the input npz file from Task 2", required=True)
@@ -265,12 +308,15 @@ if __name__ == "__main__":
     
     feats = data[:,:-1]
     label = data[:,-1].ravel()
-    X_train, y_train, X_test, y_test = train_test_split(feats, label, test_size = 0.2, 
+    X_train, X_test, y_train, y_test = train_test_split(feats, label, test_size = 0.2, 
                                                         shuffle=True, random_state=401)
     # TODO : complete each classification experiment, in sequence. (-)
-    iBest = class31(args.output_dir, x_train, x_test, y_train, y_test)
-    X_1k, y_1k = class32(args.output_dir, x_train, x_test, y_train, y_test, iBest)
-    class33(args.output_dir, x_train, x_test, y_train, y_test, iBest, X_1k, y_1k)
-    # ALL DATA?@55
-    class34(args.output_dir, x_train, x_test, y_train, y_test, iBest)
+    #iBest = class31(args.output_dir, X_train, X_test, y_train, y_test)
+    #print(iBest)
+    iBest = 5
+    X_1k, y_1k = class32(args.output_dir, X_train, X_test, y_train, y_test, iBest)
+    
+    class33(args.output_dir, X_train, X_test, y_train, y_test, iBest, X_1k, y_1k)
+
+    class34(args.output_dir, X_train, X_test, y_train, y_test, iBest)
 
